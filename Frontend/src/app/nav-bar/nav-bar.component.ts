7ba0dac0d66f88149a93247a133316d102cf08b4
Beha <kerim.beha12@gmail.com>
2024-11-27 13:20:51 +0100

    fixevi za login i dodavanje htmla za register i edit profila

--- Frontend/src/app/nav-bar/nav-bar.component.ts
+++ Frontend/src/app/nav-bar/nav-bar.component.ts
@@ -4,19 +4,24 @@ import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 import { AccountService } from '../services/account.service';
 import { FormsModule } from '@angular/forms';
-import {CommonModule} from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterModule,MatIconModule,MatButtonModule,FormsModule, CommonModule],
+  imports: [RouterModule, MatIconModule, MatButtonModule, FormsModule, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
   loggedIn = false;
   private accountService = inject(AccountService);
-  body:any = {};
+  body: any = {};
   isLoginFormVisible: boolean = false;
+  errorMessage: string = "";
+
+
+
+
   openForm(): void {
     console.log("Opening login form...");
     this.isLoginFormVisible = true;
@@ -26,15 +31,19 @@ export class NavBarComponent {
     console.log("Closing login form...");
     this.isLoginFormVisible = false;
   }
-  login(){
+  login() {
     this.accountService.login(this.body).subscribe({
       next: response => {
         console.log(response);
         this.loggedIn = true;
+        this.errorMessage = "";
+        this.closeForm();
       },
-        error: error => console.log(error)
-    })
-  }
+      error: error => {console.log(error);
+      this.errorMessage = "Login failed. Please check your inputs";
+      }
+    });
+  };
 }
 /*
 const loginFormContainer = document.querySelector(".loginForm-container"),
