0e86ae1b97a1cf088354900ce002f4a670c4fdf7
Beha_404 <kerim.beha12@gmail.com>
2025-01-07 19:02:11 +0100

    Connection setup
    #46

---
+++ backend/DTOs/UserDto.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Backend.Dtos;
+
+public class UserDto
+{
+    public  string? Username { get; set; }
+    public  string? Token { get; set; }
+    public  byte[]? PasswordHash { get; set; }
+    public  byte[]? PasswordSalt { get; set; }
+    public  string Password { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? City { get; set; }
+    public string? State { get; set; }
+    public string? Country { get; set; }
+    public string? profilePicture { get; set; }
+    public string? PostalCode { get; set; }
+    public string? Address { get; set; }
+}
