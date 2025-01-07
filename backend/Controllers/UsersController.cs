94a434bd6e07f58b960f22b56b12e2c12dc81487
Beha_404 <kerim.beha12@gmail.com>
2025-01-07 19:33:15 +0100

    User endpoints
    #43

---
+++ backend/Controllers/UsersController.cs
@@ -0,0 +1,67 @@
+using Backend.Data;
+using Backend.Dtos;
+using Backend.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Controllers;
+public class UsersController(DataContext context) : BaseApiController
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        if (!users.Any()) return NotFound("No users found");
+        return users;
+    }
+
+    [HttpGet("{name}")]
+    public async Task<ActionResult<User>> GetUser(string name)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == name.ToLower());
+        if (user == null) return NotFound("No user with this name");
+        return user;
+    }
+
+    [HttpDelete("{name}")]
+    public async Task<ActionResult<User>> DeleteUser(string name)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == name.ToLower());
+        if (user == null) return NotFound("No user with this name");
+        context.Users.Remove(user);
+        await context.SaveChangesAsync();
+        return user;
+    }
+
+    [HttpDelete("all")]
+    public async Task<ActionResult<User>> DeleteAllUsers()
+    {
+        var users = await context.Users.ToListAsync();
+        if (!users.Any()) return NotFound("No users found");
+        context.RemoveRange(users);
+        await context.SaveChangesAsync();
+        return Ok("Done");
+    }
+
+
+    [HttpPut("{name}")]
+    public async Task<ActionResult> UpdateUser(string name, User newUser)
+    {
+
+        var User = await context.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == name.ToLower());
+        if (User == null) return NotFound("No user with this name");
+        User.Username = newUser.Username ?? User.Username;
+        User.FirstName = newUser.FirstName ?? User.FirstName;
+        User.LastName = newUser.LastName ?? User.LastName;
+        User.Country = newUser.Country ?? User.Country;
+        User.Address = newUser.Address ?? User.Address;
+        User.PostalCode = newUser.PostalCode ?? User.PostalCode;
+        User.State = newUser.State ?? User.State;
+        User.City = newUser.City ?? User.City;
+
+        await context.SaveChangesAsync();
+        return Ok();
+    }
+}
--- backend/DTOs/UserDto.cs
+++ backend/DTOs/UserDto.cs
@@ -6,8 +6,6 @@ public class UserDto
 {
     public  string? Username { get; set; }
     public  string? Token { get; set; }
-    public  byte[]? PasswordHash { get; set; }
-    public  byte[]? PasswordSalt { get; set; }
     public  string? Password { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
