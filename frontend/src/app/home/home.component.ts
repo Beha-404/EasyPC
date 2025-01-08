31039a1e32ff8ddb37ec008ac5da3ccae4ca0c03
Beha_404 <kerim.beha12@gmail.com>
2025-01-08 18:22:46 +0100

    update-profile
    #50

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
+import { AccountService } from '../_services/account.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,28 @@ import { NavBarComponent } from "../nav-bar/nav-bar.component";
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+ http = inject(HttpClient);
+  accountService = inject(AccountService);
+  users: any;
 
+  ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  getUsers(){
+    this.http.get('http://localhost:5132/api/users').subscribe(
+      {
+        next: respoonse => this.users = respoonse
+      }
+    )
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
 }
