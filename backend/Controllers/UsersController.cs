e1869c7006274b9caf606ac96de9ee287cdfe0c9
Beha_404 <kerim.beha12@gmail.com>
2025-01-09 18:33:02 +0100

    upload picture
    #54

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -48,22 +48,31 @@ public class UsersController(DataContext context) : BaseApiController
 
     
 
-    [HttpPut("{name}")]
-    public async Task<ActionResult> UpdateUser(string name, User newUser)
+   [HttpPut("{name}")]
+public async Task<ActionResult> UpdateUser(string name, IFormCollection form)
+{
+    var user = await context.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == name.ToLower());
+    if (user == null) return NotFound("No user with this name");
+
+    user.Username = form["Username"].FirstOrDefault() ?? user.Username;
+    user.FirstName = form["FirstName"].FirstOrDefault() ?? user.FirstName;
+    user.LastName = form["LastName"].FirstOrDefault() ?? user.LastName;
+    user.Country = form["Country"].FirstOrDefault() ?? user.Country;
+    user.Address = form["Address"].FirstOrDefault() ?? user.Address;
+    user.PostalCode = form["PostalCode"].FirstOrDefault() ?? user.PostalCode;
+    user.State = form["State"].FirstOrDefault() ?? user.State;
+    user.City = form["City"].FirstOrDefault() ?? user.City;
+    
+     if (form.Files.Any())
     {
-
-        var User = await context.Users.FirstOrDefaultAsync(x => x.Username.ToLower() == name.ToLower());
-        if (User == null) return NotFound("No user with this name");
-        User.Username = newUser.Username ?? User.Username;
-        User.FirstName = newUser.FirstName ?? User.FirstName;
-        User.LastName = newUser.LastName ?? User.LastName;
-        User.Country = newUser.Country ?? User.Country;
-        User.Address = newUser.Address ?? User.Address;
-        User.PostalCode = newUser.PostalCode ?? User.PostalCode;
-        User.State = newUser.State ?? User.State;
-        User.City = newUser.City ?? User.City;
-
-        await context.SaveChangesAsync();
-        return Ok();
+        var file = form.Files.First();
+        using var memoryStream = new MemoryStream();
+        await file.CopyToAsync(memoryStream);
+        user.profilePicture = memoryStream.ToArray();
     }
+
+    await context.SaveChangesAsync();
+    return Ok();
+}
+
 }
